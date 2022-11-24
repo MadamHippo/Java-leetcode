780b6edb7e86e9b4647ee7d571772666211f5204
Lily <88348716+MadamHippo@users.noreply.github.com>
2022-11-24 01:04:11 -0800

    Create Hackerrank-java-strings-intro.java

---
+++ Hackerrank-java-strings-intro.java
@@ -0,0 +1,60 @@
+/*
+
+This exercise is to test your understanding of Java Strings. A sample String declaration:
+
+String myString = "Hello World!"
+The elements of a String are called characters. The number of characters in a String is called the length, and it can be retrieved with the String.length() method.
+
+Given two strings of lowercase English letters,  and , perform the following operations:
+
+Sum the lengths of  A and B.
+Determine if  is lexicographically larger than  (i.e.: does  come before  in the dictionary?).
+Capitalize the first letter in  and  and print them on a single line, separated by a space.
+
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        
+        /* Enter your code here. Print output to STDOUT. */
+        
+        
+        //  Sum of A + B
+        int sum = A.length() + B.length();
+        System.out.println(sum);
+        
+        
+        // For the second line, write Yes if A is lexicographically greater than  B otherwise print No instead.
+        if (A.compareTo(B) > 0) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+        
+        
+        // Capitalize the first letter in  A and B and print them on a single line, separated by a space.
+        // Getting Characters by Index from a String. The index of the first character is 0; the index of the last is length()-1
+        A = A.substring(0, 1).toUpperCase()+A.substring(1);
+        // System.out.println(A);
+        
+        B = B.substring(0,1).toUpperCase()+B.substring(1);
+        
+        // System.out.println(B);
+
+        System.out.println(A + " " + B);
+        
+        
+        
+    }
+}
+
+
+
