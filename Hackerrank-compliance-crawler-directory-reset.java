fa87145ed67bd22c7443f34701797161fed22b67
Lily <88348716+MadamHippo@users.noreply.github.com>
2022-12-29 02:27:48 -0800

    Create Hackerrank-compliance-crawler-directory-reset.java

---
+++ Hackerrank-compliance-crawler-directory-reset.java
@@ -0,0 +1,42 @@
+class Result {
+
+    /*
+    Compliance Crawler Directory Reset
+    
+     * Complete the 'minimumSteps' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts an Array(List) of Strings loggedMoves as parameter.
+     
+     Also named...minimum-number-of-operations-required-to-return-to-the-root-folder
+     
+     */
+
+    public static int minimumSteps(List<String> loggedMoves) {
+    // Write your code here
+    // Approach: The problem can be solved using Stack. Follow the steps below to solve the problem:
+    
+    
+    // .equal means..do they have the same value. Do the object holds the same value.
+    // == means do they point to the same instances.
+    
+    int minMoves = 0;
+    
+    Stack<String> stackie = new Stack<String>();
+    
+    for (int i = 0; i < loggedMoves.size(); i++){
+        if (loggedMoves.get(i).equals("../") && !stackie.isEmpty()){
+            stackie.pop();
+            minMoves--;
+        } else if (loggedMoves.get(i).equals("./")) {
+            continue;
+        } else {
+            stackie.push(loggedMoves.get(i));
+            minMoves++;
+        }
+    }
+    return minMoves;
+
+    }
+
+}
