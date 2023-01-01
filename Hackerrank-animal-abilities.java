b134b228f3ffdae4a7276e8c1185e63356f45d54
Lily <88348716+MadamHippo@users.noreply.github.com>
2023-01-01 03:21:48 -0800

    Create Hackerrank-animal-abilities.java

---
+++ Hackerrank-animal-abilities.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+// how to implement multiple interfaces with a Class
+
+interface Animal {
+    public void eat();
+    public void makeSound();
+}
+
+
+interface Bird {
+    int legs = 2;
+    public void fly();
+}
+
+class Parrot implements Animal, Bird {
+    public void eat(){
+        System.out.println("Parrots can eat up to 100 gms in a day");
+    }
+    
+    public void makeSound(){
+        System.out.println("Parrots make sound of screech");
+    }
+    
+    public void fly() {
+        System.out.println("Parrots can fly up to 50 miles in a day");
+    }
+}
+
+public class Solution{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            String name = sc.nextLine();
+            Parrot p = new Parrot();
+            if(name.equals("eat")){
+                p.eat();
+            }
+            if(name.equals("makeSound")){
+                p.makeSound();
+            }
+            if(name.equals("fly")){
+                p.fly();
+            }
+        }
+    }
+}
