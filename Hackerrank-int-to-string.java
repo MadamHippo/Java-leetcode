b10c8b920eff4ccb10a4e26317d8bd35e5c266b8
Lily <88348716+MadamHippo@users.noreply.github.com>
2023-01-23 01:50:44 -0800

    Create Hackerrank-int-to-string.java

---
+++ Hackerrank-int-to-string.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.security.*;
+
+
+public class Solution {
+ public static void main(String[] args) {
+
+  DoNotTerminate.forbidExit();
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+
+  
+    
+   //Write your code here
+   String s = Integer.toString(n);
+
+   
