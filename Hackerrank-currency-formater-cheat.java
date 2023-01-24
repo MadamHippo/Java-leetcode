d56925cd8664a05af078fcb7da5ee2192ff5e154
Lily <88348716+MadamHippo@users.noreply.github.com>
2023-01-24 00:03:23 -0800

    Create Hackerrank-currency-formater-cheat.java

---
+++ Hackerrank-currency-formater-cheat.java
@@ -0,0 +1,27 @@
+// Not my code, and using built-ins...because this is an annoying problem and built ins exist for a reason.
+
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class JavaCurrencyFormatter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        //Importing NumberFormat Java Class
+        NumberFormat us = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat china = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        NumberFormat france = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+
+        Locale locale = new Locale("en", "in");
+        NumberFormat india = NumberFormat.getCurrencyInstance(locale);
+
+        System.out.println("US: " + us.format(payment));
+        System.out.println("India: " + india.format(payment));
+        System.out.println("China: " + china.format(payment));
+        System.out.print("France: " + france.format(payment));
+    }
+}
