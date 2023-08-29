28034f62312f9d7efb7e7f5875e8d859ed72f29c
LilyTheDev <88348716+MadamHippo@users.noreply.github.com>
2023-08-28 23:10:21 -0700

    Update Per-scholas-star-diamond-pattern.java

--- Per-scholas-star-diamond-pattern.java
+++ Per-scholas-star-diamond-pattern.java
@@ -8,41 +8,63 @@ Enter the number of rows as an input. Now we use for loops to print two equiater
 */
 
 import java.util.Scanner;
-public class PracticeQuestion4
-{
-    public static void main(String args[])
-    {
+
+public class PracticeQuestion4 {
+    public static void main(String args[]) {
         int n, i, j, space = 1;
+        
+        // Prompt the user to enter the number of rows
         System.out.print("Enter the number of rows: ");
+        
+        // Create a Scanner object to read input from the user
         Scanner s = new Scanner(System.in);
+        
+        // Read the number of rows provided by the user
         n = s.nextInt();
+        
+        // Calculate the initial number of spaces
         space = n - 1;
-        for (j = 1; j <= n; j++)
-        {
-            for (i = 1; i <= space; i++)
-            {
+        
+        // Loop to print the upper half of the pattern
+        for (j = 1; j <= n; j++) {
+            // Print spaces before the asterisks
+            for (i = 1; i <= space; i++) {
                 System.out.print(" ");
             }
+            
+            // Decrease the number of spaces for the next row
             space--;
-            for (i = 1; i <= 2 * j - 1; i++)
-            {
+            
+            // Print asterisks in a pyramid pattern
+            for (i = 1; i <= 2 * j - 1; i++) {
                 System.out.print("*");
             }
+            
+            // Move to the next line after printing a row
             System.out.println("");
         }
+        
+        // Reset the value of the space variable
         space = 1;
-        for (j = 1; j <= n - 1; j++)
-        {
-            for (i = 1; i <= space; i++)
-            {
+        
+        // Loop to print the lower half of the pattern
+        for (j = 1; j <= n - 1; j++) {
+            // Print spaces before the asterisks
+            for (i = 1; i <= space; i++) {
                 System.out.print(" ");
             }
+            
+            // Increase the number of spaces for the next row
             space++;
-            for (i = 1; i <= 2 * (n - j) - 1; i++)
-            {
+            
+            // Print asterisks in an inverted pyramid pattern
+            for (i = 1; i <= 2 * (n - j) - 1; i++) {
                 System.out.print("*");
             }
+            
+            // Move to the next line after printing a row
             System.out.println("");
         }
     }
 }
+
