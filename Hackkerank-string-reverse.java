d668c8d565228aad1a097629c7c4326d1af05983
LilyTheDev <88348716+MadamHippo@users.noreply.github.com>
2023-10-20 13:55:25 -0700

    Update Hackkerank-string-reverse.java

--- Hackkerank-string-reverse.java
+++ Hackkerank-string-reverse.java
@@ -1,6 +1,4 @@
-/* A palindrome is a word, phrase, number, or other sequence of characters which reads the same backward or forward.
-
-Given a string , print Yes if it is a palindrome, print No otherwise.
+/* Problem: A palindrome is a word, phrase, number, or other sequence of characters that reads the same backward or forward. Given a string , print Yes if it is a palindrome, print No otherwise.
 */
 
 import java.io.*;
